27f8fefd99b58016c4fdd4b80f5fdc7518e29f8e
Matthew Wojnicki <mwojnicki@comcast.net>
2023-02-23 12:54:53 -0600

    updated mod description

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,8 +6,9 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Soy Boy";
 
-    public const string Description = "Adds Soy Boy";
+    public const string Description = 
+        "Adds Soy Boy as a new tower. This is my first BTD 6 mod so bare with it. Soy Boy currently only has one upgrade path but I plan to add the other two upgrade paths real soon.";
 
-    public const string RepoOwner = "LynxCGames"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "Soy-Boy-Mod"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "LynxCGames";
+    public const string RepoName = "Soy-Boy-Mod";
 }
